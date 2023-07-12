1fb611c3c1938879ac05068380ada5275877a913
jprly <52832819+jprly@users.noreply.github.com>
2023-07-12 17:04:03 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/GoCD/i);
+  const linkElement = screen.getByText(/Jenkins/i);
   expect(linkElement).toBeInTheDocument();
 });
