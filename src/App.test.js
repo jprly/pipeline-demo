53b8cf628348e7ce30ef79eff96df13911c00870
jprly <52832819+jprly@users.noreply.github.com>
2023-07-12 17:04:33 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders deployed by Jenkins text', () => {
   render(<App />);
   const linkElement = screen.getByText(/Jenkins/i);
   expect(linkElement).toBeInTheDocument();
