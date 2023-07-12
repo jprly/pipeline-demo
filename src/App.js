f109aa402e0be79b3fdd854cf3d8f607538cea9b
jprly <52832819+jprly@users.noreply.github.com>
2023-07-12 17:03:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployed by GoCD Pipeline!
+          Deployed by Jenkins Pipeline!
         </p>
       </header>
     </div>
